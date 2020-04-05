e004fb880ecd36895f7a0c5c7b2fc7d03f595c9e
jeromeboy228 <egorbaranov77@gmail.com>
2020-04-05 21:55:08 +0300

    main add

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Основная страница</title>
+</head>
+<body>
+<br>
+    <form action="input_form.php" method="post">
+    Добавление пребора <br>
+    <input type="submit" value="Перейти">
+    </form>
+    <br>
+    Список приборов
+    <form action="output_form.php" method="post">
+    <input type="submit" value="Перейти">
+    </form>
+</body>
+</html>
